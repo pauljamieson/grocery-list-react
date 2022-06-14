df63a88a46c04847ae486eae14f21e9223dc44bd
Paul Jamieson <36163469+pauljamieson@users.noreply.github.com>
2022-06-13 23:01:50 -0400

    Simple layout set

---
+++ src/layouts/Main.js
@@ -0,0 +1,11 @@
+const Main = (props) => {
+  return (
+    <div className="app-root">
+      <div className="app-header">Header</div>
+      <div className="app-main">Main</div>
+      <div className="app-footer">Footer</div>
+    </div>
+  );
+};
+
+export default Main;
